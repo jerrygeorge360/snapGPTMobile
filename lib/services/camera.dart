ad2bced5ad0fe026ca0a3a7cce442f6ac94b5270
jerry george <jerrygithub360@gmail.com>
2023-12-22 12:28:06 +0100

    created a service to send the image data to the backend

--- lib/services/camera.dart
+++ lib/services/camera.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:http/http.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:convert' as convert;
 
 Future<dynamic> pickImage() async{
   try{
@@ -23,10 +25,29 @@ Future<dynamic> pickCamera() async{
     if(image == null) return;
     final imageTemporary = File(image.path);
 
-    return imageTemporary;
+    return imageTemporary.readAsBytes();
   }
   on PlatformException catch(e){
     print('Failed to pick image: $e');
+  }
+
+}
+
+Future<Map<String,dynamic>> sendImageData(String route,Map<String,dynamic>data)async{
+  try{
+    Map<String,dynamic> requestData=data;
+    var request=await post(Uri.parse(route),body: requestData,);
+    if(request.statusCode==200){
+      Map<String,dynamic> requestDecoded=convert.jsonDecode(request.body) as Map<String,dynamic>;
+      return requestDecoded;
+    }
+    else{
+      throw Exception('failed to send request. Status cod: ${request.body}');
+    }
+  }
+  catch(e){
+    print('Error');
+    rethrow;
   }
 
 }
